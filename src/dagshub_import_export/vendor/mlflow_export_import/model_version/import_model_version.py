ff81eeee585b996ae12be37991d3f3c7dd16e558
KBolashev <kirill@dagshub.com>
2025-07-10 17:58:43 +0300

    More mlflow wip

--- src/dagshub_import_export/vendor/mlflow_export_import/model_version/import_model_version.py
+++ src/dagshub_import_export/vendor/mlflow_export_import/model_version/import_model_version.py
@@ -11,36 +11,36 @@ from dagshub_import_export.vendor.mlflow_export_import.common.click_options impo
     opt_input_dir,
     opt_model,
     opt_import_permissions,
-    opt_import_source_tags
+    opt_import_source_tags,
 )
-from . click_options import (
-    opt_create_model,
-    opt_experiment_name,
-    opt_import_metadata,
-    opt_import_stages_and_aliases
-)
-from dagshub_import_export.vendor.mlflow_export_import.common import MlflowExportImportException
+from .click_options import opt_create_model, opt_experiment_name, opt_import_metadata, opt_import_stages_and_aliases
 from dagshub_import_export.vendor.mlflow_export_import.common import utils, io_utils, mlflow_utils, model_utils
 from dagshub_import_export.vendor.mlflow_export_import.common.mlflow_utils import MlflowTrackingUriTweak
-from dagshub_import_export.vendor.mlflow_export_import.common.source_tags import set_source_tags_for_field, fmt_timestamps
+from dagshub_import_export.vendor.mlflow_export_import.common.source_tags import (
+    set_source_tags_for_field,
+    fmt_timestamps,
+)
 from dagshub_import_export.vendor.mlflow_export_import.common.timestamp_utils import format_seconds
 from dagshub_import_export.vendor.mlflow_export_import.run.import_run import import_run
-from dagshub_import_export.vendor.mlflow_export_import.client.client_utils import create_mlflow_client, create_dbx_client
+from dagshub_import_export.vendor.mlflow_export_import.client.client_utils import (
+    create_mlflow_client,
+    create_dbx_client,
+)
 
 _logger = utils.getLogger(__name__)
 
 
 def import_model_version(
-        model_name,
-        experiment_name,
-        input_dir,
-        create_model = False,
-        import_permissions = False,
-        import_source_tags = False,
-        import_stages_and_aliases = True,
-        import_metadata = False,
-        mlflow_client = None
-    ):
+    model_name,
+    experiment_name,
+    input_dir,
+    create_model=False,
+    import_permissions=False,
+    import_source_tags=False,
+    import_stages_and_aliases=True,
+    import_metadata=False,
+    mlflow_client=None,
+):
     """
     Exports a model version.
 
@@ -70,10 +70,10 @@ def import_model_version(
 
     path = os.path.join(input_dir, "run")
     dst_run, _ = import_run(
-        input_dir = path,
-        experiment_name = experiment_name,
-        import_source_tags = import_source_tags,
-        mlflow_client = mlflow_client
+        input_dir=path,
+        experiment_name=experiment_name,
+        import_source_tags=import_source_tags,
+        mlflow_client=mlflow_client,
     )
 
     if create_model:
@@ -88,28 +88,23 @@ def import_model_version(
     dst_source = f"{dst_run.info.artifact_uri}/{model_path}"
     dst_vr = _import_model_version(
         mlflow_client,
-        model_name = model_name,
-        src_vr = src_vr,
-        dst_run_id = dst_run.info.run_id,
-        dst_source = dst_source,
-        import_stages_and_aliases = import_stages_and_aliases,
-        import_source_tags = import_source_tags
+        model_name=model_name,
+        src_vr=src_vr,
+        dst_run_id=dst_run.info.run_id,
+        dst_source=dst_source,
+        import_stages_and_aliases=import_stages_and_aliases,
+        import_source_tags=import_source_tags,
     )
     return dst_vr
 
+
 def _import_model_version(
-        mlflow_client,
-        model_name,
-        src_vr,
-        dst_run_id,
-        dst_source,
-        import_stages_and_aliases = True,
-        import_source_tags = False
-    ):
+    mlflow_client, model_name, src_vr, dst_run_id, dst_source, import_stages_and_aliases=True, import_source_tags=False
+):
     start_time = time.time()
-    dst_source = dst_source.replace("file://","") # OSS MLflow
-    if not dst_source.startswith("dbfs:") and not os.path.exists(dst_source):
-        raise MlflowExportImportException(f"'source' argument for MLflowClient.create_model_version does not exist: {dst_source}", http_status_code=404)
+    dst_source = dst_source.replace("file://", "")  # OSS MLflow
+    # if not dst_source.startswith("dbfs:") and not os.path.exists(dst_source):
+    #     raise MlflowExportImportException(f"'source' argument for MLflowClient.create_model_version does not exist: {dst_source}", http_status_code=404)
 
     tags = src_vr["tags"]
     if import_source_tags:
@@ -121,23 +116,19 @@ def _import_model_version(
     _logger.info(f"Importing model version '{model_name}'")
     with MlflowTrackingUriTweak(mlflow_client):
         dst_vr = mlflow_client.create_model_version(
-            name = model_name,
-            source = dst_source,
-            run_id = dst_run_id,
-            description = src_vr.get("description"),
-            tags = tags
+            name=model_name, source=dst_source, run_id=dst_run_id, description=src_vr.get("description"), tags=tags
         )
 
     if import_stages_and_aliases:
-        for alias in src_vr.get("aliases",[]):
+        for alias in src_vr.get("aliases", []):
             mlflow_client.set_registered_model_alias(dst_vr.name, alias, dst_vr.version)
 
         if not model_utils.is_unity_catalog_model(model_name):
             src_current_stage = src_vr["current_stage"]
-            if src_current_stage and src_current_stage != "None": # fails for Databricks  but not OSS
+            if src_current_stage and src_current_stage != "None":  # fails for Databricks  but not OSS
                 mlflow_client.transition_model_version_stage(model_name, dst_vr.version, src_current_stage)
 
-    dur = format_seconds(time.time()-start_time)
+    dur = format_seconds(time.time() - start_time)
     _logger.info(f"Imported model version '{model_name}/{dst_vr.version}' in {dur}")
     return mlflow_client.get_model_version(dst_vr.name, dst_vr.version)
 
@@ -160,7 +151,7 @@ def _extract_model_path(source):
     idx = source.find(pattern)
     if idx == -1:
         return None
-    return source[1+idx+len(pattern):]
+    return source[1 + idx + len(pattern) :]
 
 
 def _set_source_tags_for_field(dct, tags):
@@ -178,23 +169,31 @@ def _set_source_tags_for_field(dct, tags):
 @opt_import_source_tags
 @opt_import_stages_and_aliases
 @opt_import_metadata
-
-def main(input_dir, model, experiment_name, create_model, import_permissions, import_source_tags, import_stages_and_aliases, import_metadata):
+def main(
+    input_dir,
+    model,
+    experiment_name,
+    create_model,
+    import_permissions,
+    import_source_tags,
+    import_stages_and_aliases,
+    import_metadata,
+):
     """
     Imports a registered model version and its run.
     """
     _logger.info("Options:")
-    for k,v in locals().items():
+    for k, v in locals().items():
         _logger.info(f"  {k}: {v}")
     import_model_version(
-        model_name = model,
-        experiment_name = experiment_name,
-        input_dir = input_dir,
-        create_model = create_model,
-        import_permissions = import_permissions,
-        import_source_tags = import_source_tags,
-        import_stages_and_aliases = import_stages_and_aliases,
-        import_metadata = import_metadata
+        model_name=model,
+        experiment_name=experiment_name,
+        input_dir=input_dir,
+        create_model=create_model,
+        import_permissions=import_permissions,
+        import_source_tags=import_source_tags,
+        import_stages_and_aliases=import_stages_and_aliases,
+        import_metadata=import_metadata,
     )
 
 
